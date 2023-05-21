3fff73722ac37c6f67c0cee2b7e7cd477f29d287
Tanzhor_RIK <tanzhor4@gmail.com>
2023-05-21 21:18:49 +0700

    add exceptions

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import os
 from googleapiclient.discovery import build
+import googleapiclient.errors
 
 
 class Video:
@@ -7,13 +8,24 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id):
-        self.video_id = video_id
-        self.video_info = self.youtube.videos().list(id=self.video_id,
-                                                     part='snippet,statistics').execute()
-        self.title = self.video_info['items'][0]['snippet']['title']
-        self.url = 'https://youtu.be/' + self.video_id
-        self.views = self.video_info['items'][0]['statistics']['viewCount']
-        self.likes = self.video_info['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.url = 'https://youtu.be/' + self.video_id
+            self.video_info = self.youtube.videos().list(id=self.video_id,
+                                                         part='snippet,statistics').execute()
+            self.title = self.video_info['items'][0]['snippet']['title']
+
+        except (googleapiclient.errors.HttpError, IndexError):
+            self.title = None
+            self.url = None
+            self.views = None
+            self.like_count = None
+
+        else:
+            self.views = self.video_info['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_info['items'][0]['statistics'][
+                'likeCount']
+            print(video_id)
 
     def __str__(self):
         return self.title
